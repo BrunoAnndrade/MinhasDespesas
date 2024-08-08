f5d9a2bf924ec180308bd3c9115ad21922465987
Bruno Andrade <brunoandradejp@gmail.com>
2024-08-08 14:30:08 -0300

    feat: add ColorEntity to appDataBase

--- app/src/main/java/com/example/minhasdespesas/data/AppDataBase.kt
+++ app/src/main/java/com/example/minhasdespesas/data/AppDataBase.kt
@@ -4,21 +4,26 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.minhasdespesas.data.dao.BudgetDao
 import com.example.minhasdespesas.data.dao.CategoryDao
+import com.example.minhasdespesas.data.dao.ColorDao
 import com.example.minhasdespesas.data.dao.ExpenseDao
 import com.example.minhasdespesas.data.entity.BudgetEntity
 import com.example.minhasdespesas.data.entity.CategoryEntity
+import com.example.minhasdespesas.data.entity.ColorEntity
 import com.example.minhasdespesas.data.entity.ExpenseEntity
 
 @Database(
     entities = [
         ExpenseEntity::class,
         BudgetEntity::class,
-        CategoryEntity::class],
+        CategoryEntity::class,
+        ColorEntity::class
+    ],
     version = 3
 )
 abstract class AppDataBase : RoomDatabase() {
     abstract fun expenseDao(): ExpenseDao
     abstract fun budgetDao(): BudgetDao
     abstract fun categoryDao(): CategoryDao
+    abstract fun colorDao(): ColorDao
 
 }
