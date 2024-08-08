f3755c1b668772c1db358c792a3fe0f874d4cc06
Bruno Andrade <brunoandradejp@gmail.com>
2024-08-08 14:20:41 -0300

    Feature: Create ColorEntity

---
+++ app/src/main/java/com/example/minhasdespesas/data/entity/ColorEntity.kt
@@ -0,0 +1,12 @@
+package com.example.minhasdespesas.data.entity
+
+import androidx.compose.ui.graphics.Color
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "colors")
+data class ColorEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val colorHex: String 
+)
--- app/src/main/java/com/example/minhasdespesas/presentation/screens/detail/ExpenseDetailViewModel.kt
+++ app/src/main/java/com/example/minhasdespesas/presentation/screens/detail/ExpenseDetailViewModel.kt
@@ -34,21 +34,7 @@ class ExpenseDetailViewModel @Inject constructor(
         }
     }
 
-    val listColors = listOf(
-        Color(0xFFE57373),
-        Color(0xFFF06292),
-        Color(0xFFBA68C8),
-        Color(0xFF9575CD),
-        Color(0xFF7986CB),
-        Color(0xFF64B5F6),
-        Color(0xFF4FC3F7),
-        Color(0xFF4DD0E1),
-        Color(0xFF4DB6AC),
-        Color(0xFF81C784),
-        Color(0xFFAED581),
-        Color(0xFFFF8A65),
-        Color(0xFFFFA726),
-    )
+
 
     fun editAndSaveExpense(
         expenseName: String,
