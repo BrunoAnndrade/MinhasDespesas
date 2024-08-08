fb4e6ab9ef626bd654de516e5b4ecc78b7eda4a4
Bruno Andrade <brunoandradejp@gmail.com>
2024-08-08 14:35:22 -0300

    refactor: Refactor all app

--- app/src/main/java/com/example/minhasdespesas/data/AppDataBase.kt
+++ app/src/main/java/com/example/minhasdespesas/data/AppDataBase.kt
@@ -4,11 +4,9 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.minhasdespesas.data.dao.BudgetDao
 import com.example.minhasdespesas.data.dao.CategoryDao
-import com.example.minhasdespesas.data.dao.ColorDao
 import com.example.minhasdespesas.data.dao.ExpenseDao
 import com.example.minhasdespesas.data.entity.BudgetEntity
 import com.example.minhasdespesas.data.entity.CategoryEntity
-import com.example.minhasdespesas.data.entity.ColorEntity
 import com.example.minhasdespesas.data.entity.ExpenseEntity
 
 @Database(
@@ -16,7 +14,6 @@ import com.example.minhasdespesas.data.entity.ExpenseEntity
         ExpenseEntity::class,
         BudgetEntity::class,
         CategoryEntity::class,
-        ColorEntity::class
     ],
     version = 3
 )
@@ -24,6 +21,4 @@ abstract class AppDataBase : RoomDatabase() {
     abstract fun expenseDao(): ExpenseDao
     abstract fun budgetDao(): BudgetDao
     abstract fun categoryDao(): CategoryDao
-    abstract fun colorDao(): ColorDao
-
 }
