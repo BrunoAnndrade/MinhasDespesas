a6fa2af5b2ce9831538645632c77c4f24dedfb0e
Bruno Andrade <brunoandradejp@gmail.com>
2024-08-20 13:32:31 -0300

    Style(): update text field colors

--- app/src/main/java/com/example/minhasdespesas/presentation/screens/detail/DatePickerContent.kt
+++ app/src/main/java/com/example/minhasdespesas/presentation/screens/detail/DatePickerContent.kt
@@ -13,8 +13,10 @@ import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -24,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.datetime.Instant
@@ -45,9 +48,17 @@ fun DatePickerContent(
     val state = rememberDatePickerState()
 
     Row {
-        TextField(
+        OutlinedTextField(
             value = date,
             onValueChange = {},
+            label = { Text("Data") },
+            colors = TextFieldDefaults.colors(
+                errorPlaceholderColor = Color.Red,
+                unfocusedContainerColor = Color.White,
+                focusedContainerColor = Color.White,
+                focusedIndicatorColor = Color.LightGray,
+                unfocusedIndicatorColor = Color.LightGray
+            ),
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f),
